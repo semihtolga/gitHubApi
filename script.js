ec406432d6d91336d5e80384fad1669a3dc10891
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2022-10-02 14:22:48 +0300

    changes2

--- script.js
+++ script.js
@@ -26,3 +26,53 @@ async function getRepos(username) {
     createErrorCard("Problem fetching repository");
   }
 }
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+        <div>
+          <img
+            src="${user.avatar_url}
+            alt="${user.name}"
+            class="avatar"
+          />
+        </div>
+        <div class="userInfo">
+          <h2>${user.name}</h2>
+          <p>${user.bio}</p>
+          <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+          </ul>
+          <div id="repos"></div>
+        </div>
+      </div>
+    `;
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(message) {
+  const cardHTML = `
+    <div class="card">
+        <h1>${message}</h1>
+    </div>
+    `;
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById("repos");
+
+  repos
+    .slice(1, 10) 
+    .forEach((repo) => {
+      const repoEl = document.createElement("a");
+      repoEl.classList.add("repo");
+      repoEl.href = repo.html_url;
+      repoEl.target = "_blank";
+      repoEl.innerText = repo.name;
+      reposEl.appendChild(repoEl);
+    });
+    console.log(reposEl);
+}
